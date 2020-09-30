7047babc4edbbed4aa3d2f4f9df00cec19bc8992
grabania <grabania.work@gmail.com>
2020-09-30 22:31:35 +0100

    Add routs in app.js

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,4 +1,8 @@
 import HomeScreen from './screens/HomeScreen.js';
+const routs = {
+	'/': HomeScreen,
+	'/product/:id': ProductScreen
+};
 const router = () => {
 	const main = document.getElementById('main-container');
 	main.innerHTML = HomeScreen.render();
